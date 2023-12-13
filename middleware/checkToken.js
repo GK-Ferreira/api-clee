a4d8c813d35837e6760e7d55060648ef2684e38b
GK-Ferreira <guilherme.ferreiradevfront@gmail.com>
2023-12-13 18:31:51 -0300

    adicionando refresh token

--- middleware/checkToken.js
+++ middleware/checkToken.js
@@ -1,23 +1,45 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
 
-function checkToken(req,res,next){
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
-  
-  
-    if(!token){
-        return res.status(401).json({msg: "acesso negado"})
-    }
-  
-    try{
-        const secret = process.env.SECRET
-        jwt.verify(token,secret)
-  
-        next()
-  
-    }catch(err){
-        res.status(400).json({msg: "Token inválido"})
+function checkToken(req, res, next) {
+  const authHeader = req.headers['authorization'];
+  const token = authHeader && authHeader.split(' ')[1];
+
+  if (!token) {
+    return res.status(401).json({ msg: 'Acesso negado' });
+  }
+
+  try {
+    const secret = process.env.SECRET;
+    const decodedToken = jwt.verify(token, secret);
+
+    req.user = decodedToken;
+    next();
+  } catch (err) {
+    if (err.name === 'TokenExpiredError') {
+      const refreshToken = req.cookies.refreshToken
+
+      if (!refreshToken) {
+        return res.status(401).json({ msg: 'Acesso negado. Token expirado e sem refresh token.' });
+      }
+
+      try {
+        const decodedRefreshToken = jwt.verify(refreshToken, process.env.REFRESH_SECRET);
+
+        const newAccessToken = jwt.sign(
+          { userId: decodedRefreshToken.userId },
+          process.env.SECRET,
+          { expiresIn: '5m' }
+        );
+        res.locals.newAccessToken = newAccessToken;
+
+        next();
+      } catch (refreshTokenError) {
+        res.status(401).json({ msg: 'Token inválido ou expirado' });
+      }
+    } else {
+      res.status(400).json({ msg: 'Token inválido' });
     }
   }
-  
-  module.exports = checkToken
+}
+
+module.exports = checkToken;
