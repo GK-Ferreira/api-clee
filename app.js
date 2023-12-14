634b71c5747142154d643d11d77affb5b3041006
GK-Ferreira <guilherme.fereiradevfront@gmail.com>
2023-12-14 13:13:29 -0300

    adiconando refresh token

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 jwt = require('jsonwebtoken')
 const cors = require('cors')
-const checkToken = require('./middleware/checkToken')
+const {checkToken, checkRefreshToken} = require('./middleware/checkToken')
 
 const equip = require('./routes/equip')
 const salas = require('./routes/salas')
@@ -121,12 +121,15 @@ app.post("/auth/login",async(req,res)=>{
          const secret = process.env.SECRET
 
          const token = jwt.sign({
-            id: user._id,
+           refreshToken
 
-          },secret,
+          },secret,{
+            expiresIn: '20s'
+          }
           )
+          const refreshToken = jwt.sign({  name,password }, secret,{expiresIn: '1800s'})
           
-          res.status(200).json({msg: "autenticação realizada com sucesso", token})
+          res.status(200).json({msg: "autenticação realizada com sucesso", token, refreshToken})
           
           
         }catch(err){
@@ -135,6 +138,56 @@ app.post("/auth/login",async(req,res)=>{
         }
         
       })
+
+      app.post("/refresh",checkRefreshToken,async(req,res)=>{
+
+        const {name,password} = req.body
+    
+    
+        if(!name){
+            return res.status(422).json("O nome é obrigatório")
+        }
+        if(!password){
+            return res.status(422).json("A senha é obrigatória")
+        }
+    
+        //check if user exists
+        const user= await User.findOne({name: name})
+    
+         if(!user){
+             return res.status(422).json({msg:"usuário não existe"})
+              
+         }
+    
+         //check password match
+         const checkPassword = await bcrypt.compare(password,user.password)
+    
+         if(!checkPassword){
+    
+            return res.status(404).json("Senha inválida")
+    
+         }
+         
+         try{
+             const secret = process.env.SECRET
+    
+             const token = jwt.sign({
+                id: user._id,
+    
+              },secret,{
+                expiresIn: '1800s'
+              }
+              )
+              
+              res.status(200).json({msg: "autenticação realizada com sucesso", token})
+              
+              
+            }catch(err){
+              console.log(err)
+              res.status(500).json({msg: "erro no server tente novamente mais tarde"})
+            }
+            
+          })
       
       app.use('/equip', equip)
       app.use('/reserva_salas',reserva_salas)
